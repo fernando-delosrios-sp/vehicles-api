40647bdf3c72f6e1840a66c09aa2e6fab38abfa4
fernando-delosrios-sp <fernando.delosrios@sailpoint.com>
2021-07-30 14:18:42 +0200

    New Eureka server

--- eureka/src/main/java/com/udacity/vehicleapi/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/udacity/vehicleapi/eureka/EurekaApplication.java
@@ -1,15 +1,15 @@
-package com.udacity.eureka;
+package com.udacity.vehicleapi.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
 @EnableEurekaServer
+@SpringBootApplication
 public class EurekaApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
 
 }
